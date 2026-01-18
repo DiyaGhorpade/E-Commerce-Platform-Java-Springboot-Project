cc59135ec1fb1bf87d750947aefce0334f0e78fa
Abhinav Singh <abhinavsinghbvb@gmail.com>
2026-01-18 15:15:34 +0530

    Docker DBs, and Auth Service with JWT

---
+++ auth-service/src/main/java/com/example/auth_service/repository/UserCredentialRepository.java
@@ -0,0 +1,10 @@
+package com.example.auth_service.repository;
+
+import com.example.auth_service.entity.UserCredential;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserCredentialRepository extends JpaRepository<UserCredential, Integer> {
+    Optional<UserCredential> findByName(String name);
+}
